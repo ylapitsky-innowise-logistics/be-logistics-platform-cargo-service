dbd0c108aa8f0a496005eaf6ab025aac1a8ee2ff
Yury Lapitski /VikaNoteBook_Win11 <yury.connect@gmail.com>
2026-06-08 16:42:50 +0300

    CargoService: init

---
+++ src/main/java/com/innowise/logistics/cargoservice/BeLogisticsPlatformCargoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.logistics.cargoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeLogisticsPlatformCargoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeLogisticsPlatformCargoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innowise/logistics/cargoservice/BeLogisticsPlatformCargoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innowise.logistics.cargoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeLogisticsPlatformCargoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
