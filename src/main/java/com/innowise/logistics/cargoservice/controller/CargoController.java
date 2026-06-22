0ab26e19d557470b3b551c8d0af1b13ae737485d
Yury Lapitski /VikaNoteBook_Win11 <yury.connect@gmail.com>
2026-06-22 17:23:34 +0300

    CargoService: поправил валидацию

--- src/main/java/com/innowise/logistics/cargoservice/controller/CargoController.java
+++ src/main/java/com/innowise/logistics/cargoservice/controller/CargoController.java
@@ -12,13 +12,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/catalog/cargos")
 @RequiredArgsConstructor
+@Validated
 public class CargoController {
 
     private final CargoService cargoService;
@@ -40,7 +47,9 @@ public class CargoController {
      * Получить выбранный товар по его ID.
      */
     @GetMapping("/{id}")
-    public ResponseEntity<CargoViewResponse> getCatalogItemById(@PathVariable Long id) {
+    public ResponseEntity<CargoViewResponse> getCatalogItemById(
+            @PathVariable Long id) {
+
         CargoViewResponse item = cargoService.getCargoById(id);
         return ResponseEntity.ok(item);
     }
@@ -52,9 +61,9 @@ public class CargoController {
     @PostMapping("/calculate-price")
     public ResponseEntity<CargoCalculationResponse> calculatePriceByIds(
             @RequestBody
-            @Valid                                                                      // @Valid на параметре – включает валидацию для @NotEmpty
-            @NotEmpty(message = "Список товаров не может быть пустым")                  // @NotEmpty – проверяет (сначала), что список не null и не пуст
-            List<@NotNull (message = "ID товара не может быть = null") Long> cargoIds) {  // List<@NotNull Long> – (во 2-ю очередь) при наличии @Valid на параметре будет проверять, что каждый элемент списка не null
+            @Valid                                                                          // @Valid на параметре – включает валидацию для @NotEmpty
+            @NotEmpty(message = "Список товаров не может быть пустым")                      // @NotEmpty – проверяет (сначала), что список не null и не пуст
+            List<@NotNull (message = "ID товара не может быть = null") Long> cargoIds) {    // List<@NotNull Long> – (во 2-ю очередь) при наличии @Valid на параметре будет проверять, что каждый элемент списка не null
 
         CargoCalculationResponse response = cargoService.calculatePriceByIds(cargoIds);
         return ResponseEntity.ok(response);
--- src/main/java/com/innowise/logistics/cargoservice/controller/ReservationController.java
+++ src/main/java/com/innowise/logistics/cargoservice/controller/ReservationController.java
@@ -14,7 +14,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -22,10 +29,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/catalog/reservations")
 @RequiredArgsConstructor
+@Validated
 public class ReservationController {
 
     private final ReservationService reservationService;
-
 
     /**
      * 1️⃣ POST /api/v1/catalog/reservations
