aa825ed7aee46e6e7ed6344233ae1070c69f1725
Yury Lapitski /VikaNoteBook_Win11 <yury.connect@gmail.com>
2026-06-23 12:00:36 +0300

    feat: поправил Cargo для новых требований задания

--- src/main/java/com/innowise/logistics/cargoservice/BeLogisticsPlatformCargoServiceApplication.java
+++ src/main/java/com/innowise/logistics/cargoservice/BeLogisticsPlatformCargoServiceApplication.java
@@ -2,10 +2,12 @@ package com.innowise.logistics.cargoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing // ⬅️ Без неё Spring Data JPA проигнорирует @CreatedDate
 @EnableJpaRepositories(basePackages = "com.innowise.logistics.cargoservice.repository")
 // Если в будущем появятся кастомные интерфейсы для Mongo, складывай их в отдельный пакет:
 @EnableMongoRepositories(basePackages = "com.innowise.logistics.cargoservice.mongo.repository")
@@ -14,5 +16,4 @@ public class BeLogisticsPlatformCargoServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BeLogisticsPlatformCargoServiceApplication.class, args);
 	}
-
 }
--- src/main/java/com/innowise/logistics/cargoservice/entity/Cargo.java
+++ src/main/java/com/innowise/logistics/cargoservice/entity/Cargo.java
@@ -2,6 +2,7 @@ package com.innowise.logistics.cargoservice.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,22 +19,23 @@ import lombok.Setter;
 import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 
 /**
  Принимаем следующее бизнес-правило: товары считаются ОДИНАКОВЫМИ, если:
-🔴 Должны СОВПАДАТЬ в рамках одного SKU
-Эти параметры описывают природу товара. Если они изменятся, это будет уже совершенно другой товар, и логисты заведут для него новый SKU.
+    🔴 Должны СОВПАДАТЬ в рамках одного SKU
+    Эти параметры описывают природу товара. Если они изменятся, это будет уже совершенно другой товар, и логисты заведут для него новый SKU.
     - name          (Наименование товара)
     - category      (Категория)
-    - dimension     (Габбаритные размеры)
-    - mongoDocId    (Фотография товара)
+    - dimension     (Габаритные размеры)
     - weight        (Вес)
 
-🟢 Могут и БУДУТ ОТЛИЧАТЬСЯ в рамках одного SKU (но товары всеравно будем считать одинаковыми)
-Эти поля описывают свойства конкретной физической единицы груза (паллеты, коробки), её жизненный цикл и её текущее положение в пространстве.
+    🟢 Могут и БУДУТ ОТЛИЧАТЬСЯ в рамках одного SKU (но товары все равно будем считать одинаковыми)
+    Эти поля описывают свойства конкретной физической единицы груза (паллеты, коробки), её жизненный цикл и её текущее положение в пространстве.
     - location (Место нахождения)
     - status и status_at (Статус)
     - created_at (Дата поступления)
@@ -41,13 +43,13 @@ import java.time.Instant;
  **/
 @Entity
 @Table(name = "cargos")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
 //@EqualsAndHashCode(exclude = "id")
-//@EqualsAndHashCode(onlyExplicitlyIncluded = true)   // Говорим Lombok: сравнивать ТОЛЬКО то, что помечено ниже
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)   // Говорим Lombok: сравнивать ТОЛЬКО то, что помечено ниже
 public class Cargo {
 
     @Id
@@ -55,38 +57,41 @@ public class Cargo {
     @Column(name = "cargo_id")
     private Long id;
 
+    // =========================================================
+    // НИЖЕ ПОЛЯ, КОТОРЫЕ УЧАСТВУЮТ В РАВЕНСТВЕ (НЕИЗМЕННЫ)
+    // =========================================================
+
     @ManyToOne(fetch = FetchType.LAZY)              // LAZY - для производительности
     @JoinColumn(name = "sku_id", nullable = false)  // sku_id - колонка внешнего ключа (FK) в таблице cargos
     @Comment("Артикул товара")
-//    @EqualsAndHashCode.Include                      // Участвует в бизнес-ключе
+    @EqualsAndHashCode.Include                      // Участвует в бизнес-ключе
     private Sku sku;
 
-    @Column(name = "mongo_doc_id")
-    @Comment("Фотография товара")
-//    @EqualsAndHashCode.Include                      // Участвует в бизнес-ключе
-    private String mongoDocId;                      // Линк на MongoDB
-
-    @Column(nullable = false)
+    @Column(name = "name", nullable = false)
     @Comment("Наименование товара")
-//    @EqualsAndHashCode.Include                      // Участвует в бизнес-ключе
+    @EqualsAndHashCode.Include                      // Участвует в бизнес-ключе
     private String name;
 
     @JdbcTypeCode(SqlTypes.NAMED_ENUM)
     @Column(name = "cargo_category", nullable = false)
     @Comment("Категория товара (электроника, книги, спорт...)")
-//    @EqualsAndHashCode.Include                      // Участвует в бизнес-ключе
+    @EqualsAndHashCode.Include                      // Участвует в бизнес-ключе
     private Category category;
 
     @Column(name = "weight", nullable = false)
     @Comment("Вес")
-//    @EqualsAndHashCode.Include                      // Участвует в бизнес-ключе
+    @EqualsAndHashCode.Include                      // Участвует в бизнес-ключе
     private Double weight;                          // в килограммах
 
     @ManyToOne(fetch = FetchType.LAZY)              // Неск. товаров могут один и тот-же габаритный размер
     @JoinColumn(name = "dimension_id", nullable = false)
     @Comment("Габаритные размеры")
-//    @EqualsAndHashCode.Include                      // Участвует в бизнес-ключе
+    @EqualsAndHashCode.Include                      // Участвует в бизнес-ключе
     private Dimension dimension;
+
+    // =========================================================
+    // НИЖЕ ПОЛЯ, КОТОРЫЕ НЕ УЧАСТВУЮТ В РАВЕНСТВЕ (МЕНЯЮТСЯ)
+    // =========================================================
 
     @Column(name = "price", nullable = false, precision = 19, scale = 2)
     @Comment("Стоимость в местной валюте")
@@ -98,10 +103,11 @@ public class Cargo {
     private Location location;
 
 
+    @CreatedDate                                    // 🟢 Автоматически выставит текущее время при сохранении через репозиторий
     @Column(name = "created_at", updatable = false, nullable = false)
     @Setter(AccessLevel.NONE)                       // Отмен. Lombok сеттер. Задается только при создании.
     @Comment("Дата поступления на склад")
-    private Instant createdAt = Instant.now();
+    private Instant createdAt;
 
     @JdbcTypeCode(SqlTypes.NAMED_ENUM)
     @Column(
@@ -116,7 +122,7 @@ public class Cargo {
     @Column(name = "status_at", nullable = false)
     @Setter(AccessLevel.NONE)                       // Отмен. Lombok сеттер. Только через updateStatus(..)
     @Comment("Дата последнего изменения статуса")
-    private Instant statusAt = Instant.now();
+    private Instant statusAt = Instant.now();       // Это защитит от NULL при первом инсерте
 
 
     // Обновление статуса и времени его установки - только через updateStatus(..)
