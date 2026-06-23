3f9988839218dcdf28f63300fc844a7136f7d849
Yury Lapitski /VikaNoteBook_Win11 <yury.connect@gmail.com>
2026-06-23 15:00:00 +0300

    feat: обновил сами таблицы согласно обновленным сущностям

--- src/main/resources/db/changelog/v001/02-create-skus-table.sql
+++ src/main/resources/db/changelog/v001/02-create-skus-table.sql
@@ -4,11 +4,21 @@
 --comment: Создание таблицы артикулов (SKU)
 
 CREATE TABLE skus(
-    sku_id BIGSERIAL,
-    name   VARCHAR(255) NOT NULL,
+    sku_id      BIGSERIAL,
+    name        VARCHAR(100)    NOT NULL,
 
-    CONSTRAINT pk_skus PRIMARY KEY (sku_id)
+    description VARCHAR(1000),
+    is_active   BOOLEAN         NOT NULL DEFAULT TRUE,
+    created_at  TIMESTAMPTZ     NOT NULL DEFAULT NOW(),
+    updated_at  TIMESTAMPTZ     NOT NULL,
+
+    CONSTRAINT pk_skus PRIMARY KEY (sku_id),
+    CONSTRAINT uq_sku_name UNIQUE (name) -- Гарантия уникальности бизнес-кода на уровне БД
 );
 
 COMMENT ON COLUMN skus.sku_id IS 'Первичный ключ артикула';
-COMMENT ON COLUMN skus.name IS 'Наименование/код артикула товара';
+COMMENT ON COLUMN skus.name IS 'Уникальный код/название артикула (бизнес-ключ)';
+COMMENT ON COLUMN skus.description IS 'Полное описание модели товара для каталога';
+COMMENT ON COLUMN skus.is_active IS 'Флаг активности артикула (доступен ли для логистики/закупок)';
+COMMENT ON COLUMN skus.created_at IS 'Дата заведения артикула в систему';
+COMMENT ON COLUMN skus.updated_at IS 'Дата последнего редактирования данных артикула';
