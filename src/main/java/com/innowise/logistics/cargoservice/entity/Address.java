0426f8fc1fb3d83ec482baca6c9ad33b40b26efc
Yury Lapitski /VikaNoteBook_Win11 <yury.connect@gmail.com>
2026-06-23 14:44:56 +0300

    feat: правка импортов

--- src/main/java/com/innowise/logistics/cargoservice/entity/Address.java
+++ src/main/java/com/innowise/logistics/cargoservice/entity/Address.java
@@ -1,7 +1,16 @@
 package com.innowise.logistics.cargoservice.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "addresses")
